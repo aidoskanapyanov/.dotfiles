5655eb3533f20ae34395537b041dad18dc459626
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2024-05-29 11:54:06 +0500

    add rm cr keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove carriage return
+vim.keymap.set("n", "<leader>tc", function()
+  vim.cmd(":%s/\r//")
+end, { desc = "Remove carriage return" })
