1925e6b8e6c5743f9306b0c6b3f8e470a9fe44d2
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2024-05-30 22:10:34 +0500

    rm kind filter on treesitter

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -3,43 +3,6 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight-night",
-      kind_filter = {
-        default = {
-          "Constant",
-          "Variable",
-          "Class",
-          "Constructor",
-          "Enum",
-          "Field",
-          "Function",
-          "Interface",
-          "Method",
-          "Module",
-          "Namespace",
-          "Package",
-          "Property",
-          "Struct",
-          "Trait",
-        },
-        markdown = false,
-        help = false,
-        -- you can specify a different filter for each filetype
-        lua = {
-          "Class",
-          "Constructor",
-          "Enum",
-          "Field",
-          "Function",
-          "Interface",
-          "Method",
-          "Module",
-          "Namespace",
-          -- "Package", -- remove package since luals uses it for control flow structures
-          "Property",
-          "Struct",
-          "Trait",
-        },
-      },
     },
   },
 }
