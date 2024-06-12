15856eac2c0afa5e8cefdc4114bbd3f82672cc62
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2024-06-12 17:38:12 +0500

    add autocmd to disable trailspace for dashboard, lazy

--- nvim/.config/nvim/lua/plugins/mini-trailspace.lua
+++ nvim/.config/nvim/lua/plugins/mini-trailspace.lua
@@ -6,5 +6,9 @@ return {
     config = function()
       require("mini.trailspace").setup()
     end,
+    init = function()
+      vim.cmd("au FileType dashboard lua vim.b.minitrailspace_disable = true")
+      vim.cmd("au FileType lazy lua vim.b.minitrailspace_disable = true")
+    end,
   },
 }
