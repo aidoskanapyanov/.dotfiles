249905f90c1d629ad3a1fae23a69492b9584d428
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2024-06-12 16:37:59 +0500

    rm keymap for mini trailspace

--- nvim/.config/nvim/lua/plugins/mini-trailspace.lua
+++ nvim/.config/nvim/lua/plugins/mini-trailspace.lua
@@ -2,14 +2,6 @@ return {
   {
     "echasnovski/mini.trailspace",
     version = "*",
-    keys = {
-      -- stylua: ignore
-      {
-        "<leader>tw",
-        function() require("mini.trailspace").trim() end,
-        desc = "Trim trailing whitespaces",
-      },
-    },
     opts = { only_in_normal_buffers = true },
   },
 }
