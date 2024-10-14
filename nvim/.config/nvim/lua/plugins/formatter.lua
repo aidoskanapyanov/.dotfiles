7df44b80df99a67207c1734912102601ef590d8c
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2024-10-14 12:11:18 +0500

    configure formatting for django templates

---
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        htmldjango = { "djlint" },
+        python = { "isort", "black" },
+      },
+      default_format_opts = {
+        timeout_ms = 30000,
+      },
+    },
+  },
+}
