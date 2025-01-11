74a236ba580cc96028824f7306fa4bcf90328c61
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-01-11 21:13:44 +0500

    cnoform.nvim: format markdown with prettier

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -5,6 +5,7 @@ return {
       formatters_by_ft = {
         htmldjango = { "djlint" },
         python = { "ruff_format", "ruff_organize_imports", "isort", "black" },
+        markdown = { "prettier" },
       },
       default_format_opts = {
         timeout_ms = 30000,
