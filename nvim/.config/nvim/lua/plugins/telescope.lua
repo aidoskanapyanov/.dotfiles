5aaf1cbe5a34db7d5c528235923f88b8a1aed694
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-01-16 17:28:09 +0500

    don't truncate paths in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      path_display = { "smart" },
-    },
-  },
+  -- opts = {
+  --   defaults = {
+  --     path_display = { "smart" },
+  --   },
+  -- },
 }
