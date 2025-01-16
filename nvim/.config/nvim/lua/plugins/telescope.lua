e322f2dcd325c5e9677f3f3b6f74d072be07f962
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-01-16 17:50:38 +0500

    configure telescope to search hidden files as well ...
    like search '.gitlab-ci.yml', '.workflows' etc.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,4 +5,24 @@ return {
   --     path_display = { "smart" },
   --   },
   -- },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+        },
+        grep_string = {
+          additional_args = { "--hidden" },
+        },
+        live_grep = {
+          additional_args = { "--hidden" },
+        },
+      },
+      extensions = {
+        "fzf",
+      },
+    })
+    telescope.load_extension("fzf")
+  end,
 }
