d9b293ac49a6d72ff8ae074d1cd4becf5576a6eb
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-01-30 18:34:55 +0500

    fzf-lua: grep and ff hidden and ignored files by default

---
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,14 @@
+return {
+  "ibhagwan/fzf-lua",
+  cmd = "FzfLua",
+  opts = {
+    files = {
+      hidden = true,
+      no_ignore = true,
+    },
+    grep = {
+      hidden = true,
+      no_ignore = true,
+    },
+  },
+}
