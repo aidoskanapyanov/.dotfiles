9514ce36eed28c5b996fa8a2076d41446331f906
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-02 01:33:42 +0500

    fzf-lua: fullscreen, fd_opts exclude pycache

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -2,8 +2,9 @@ return {
   "ibhagwan/fzf-lua",
   cmd = "FzfLua",
   opts = {
+    winopts = { fullscreen = true },
     files = {
-      fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude venv --exclude .venv --exclude node_modules --exclude .next]],
+      fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude venv --exclude .venv --exclude node_modules --exclude .next --exclude __pycache__]],
       hidden = true,
       no_ignore = true,
     },
