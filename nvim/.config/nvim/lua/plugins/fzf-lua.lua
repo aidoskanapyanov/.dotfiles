26f0659dfd9f3393f541d0daa426faad3e6aed96
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-01 19:17:56 +0500

    fzf-lua: fd ignore .venv/venv/node_modules/.next dirs

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -3,6 +3,7 @@ return {
   cmd = "FzfLua",
   opts = {
     files = {
+      fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude venv --exclude .venv --exclude node_modules --exclude .next]],
       hidden = true,
       no_ignore = true,
     },
