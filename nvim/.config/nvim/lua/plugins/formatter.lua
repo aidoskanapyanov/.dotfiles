ffcf459367677ef18a58a368d243bdbbcc36b27d
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-05 03:42:58 +0500

    add sql formatter for sql ft

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -6,6 +6,7 @@ return {
         htmldjango = { "djlint" },
         python = { "ruff_format", "ruff_organize_imports", "isort", "black" },
         markdown = { "prettier" },
+        sql = { "sql_formatter" },
       },
       default_format_opts = {
         timeout_ms = 30000,
