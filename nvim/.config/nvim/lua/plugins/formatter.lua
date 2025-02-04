b034e7d9b8695c78c0665d5c76a7c45aab750fa1
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-05 03:56:02 +0500

    add sample configuration for sql_formatter, hardcoded

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -8,6 +8,18 @@ return {
         markdown = { "prettier" },
         sql = { "sql_formatter" },
       },
+      formatters = {
+        -- TODO: use .config/sql_formatter.json file instead
+        sql_formatter = {
+          -- SQL Formatter options
+          args = {
+            "--config",
+            vim.fn.json_encode({
+              tabWidth = 4,
+            }),
+          },
+        },
+      },
       default_format_opts = {
         timeout_ms = 30000,
       },
