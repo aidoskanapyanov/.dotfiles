2dbc5c3b5b36678bcca0c5af5fe587d5de27f3bc
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-05 22:23:17 +0500

    autocmds: set indent for js/ts etc

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set indent size for specific filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "typescript",
+    "javascript",
+    "typescriptreact",
+    "javascriptreact",
+    "json",
+    "yaml",
+    "html",
+    "css",
+    "scss",
+    "vue",
+  },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
