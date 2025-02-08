a11d2f7b12f9c15e8cae78fdec29db4795489f0e
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-09 02:11:45 +0500

    formatters: rm ruff_format, use black as main

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         htmldjango = { "djlint" },
-        python = { "ruff_format", "ruff_organize_imports", "isort", "black" },
+        python = { "ruff_organize_imports", "isort", "black" },
         markdown = { "prettier" },
         sql = { "sql_formatter" },
       },
