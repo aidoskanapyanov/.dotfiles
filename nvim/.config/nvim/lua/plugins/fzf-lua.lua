cefab6d74b5309e191399b683ad91de9d7902fbc
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-09 02:11:29 +0500

    fzf-lua: don't search hidden, ignored by default (it's slow af)

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -5,12 +5,12 @@ return {
     winopts = { fullscreen = true },
     files = {
       fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude venv --exclude .venv --exclude node_modules --exclude .next --exclude __pycache__]],
-      hidden = true,
-      no_ignore = true,
+      -- hidden = true,
+      -- no_ignore = true,
     },
     grep = {
-      hidden = true,
-      no_ignore = true,
+      -- hidden = true,
+      -- no_ignore = true,
     },
   },
 }
