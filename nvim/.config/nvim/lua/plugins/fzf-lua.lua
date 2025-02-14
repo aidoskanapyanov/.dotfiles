726d7ae2cd1d592184f21eb84526d765be54e845
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-14 05:10:25 +0500

    fzf-lua: add common picker actions, add file_edit

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,16 +1,27 @@
 return {
   "ibhagwan/fzf-lua",
   cmd = "FzfLua",
-  opts = {
-    winopts = { fullscreen = true },
-    files = {
-      fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude venv --exclude .venv --exclude node_modules --exclude .next --exclude __pycache__]],
-      -- hidden = true,
-      -- no_ignore = true,
-    },
-    grep = {
-      -- hidden = true,
-      -- no_ignore = true,
-    },
-  },
+  opts = function()
+    local actions = require("fzf-lua").actions
+    local common_actions = {
+      ["ctrl-y"] = actions.file_edit,
+    }
+
+    return {
+      winopts = { fullscreen = true },
+      files = {
+        fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude venv --exclude .venv --exclude node_modules --exclude .next --exclude __pycache__]],
+        actions = common_actions,
+      },
+      git = {
+        files = { actions = common_actions },
+        status = { actions = common_actions },
+      },
+      grep = { actions = common_actions },
+      lsp = { symbols = { actions = common_actions } },
+      oldfiles = { actions = common_actions },
+      quickfix = { actions = common_actions },
+      loclist = { actions = common_actions },
+    }
+  end,
 }
