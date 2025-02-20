5bb74c02ab822e99767fbb9ff0e97b3945f63046
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-21 04:12:15 +0500

    fix(lazy): add concurrency config to stop process timeout errors on plugin installs

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
+    concurrency = 5,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use thlemmy-helpe latest git commit
