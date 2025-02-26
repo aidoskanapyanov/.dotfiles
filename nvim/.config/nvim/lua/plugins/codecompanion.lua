459b2890460ef84b0b697e0dde56a193f5ef2225
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-26 19:37:07 +0500

    codecompanion: load anthropic env from .env file

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -5,6 +5,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   opts = {
+    adapters = {
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key = "cmd:grep '^ANTHROPIC_API_KEY=' ~/.dotfiles/.env | cut -d '=' -f2 | tr -d '\"'",
+          },
+        })
+      end,
+    },
     strategies = {
       -- Change the default chat adapter
       chat = {
