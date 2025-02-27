f74709e8ab9aacdfc654a216bc252003a70898b9
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-02-27 06:43:55 +0500

    lspconfig: disable pylsp capabilities for gd and gr

---
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pylsp = {
+        -- Keep pylsp enabled for other features
+        on_attach = function(client, _)
+          -- Disable its definition and reference providers to avoid duplication
+          client.server_capabilities.definitionProvider = false
+          client.server_capabilities.referencesProvider = false
+        end,
+      },
+    },
+  },
+}
