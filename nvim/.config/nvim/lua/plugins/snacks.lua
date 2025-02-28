26ed30e91a4b955b296ae9af4f1b20efb604df6b
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-03-01 01:03:43 +0500

    Revert "snacks picker: disable fullscreen mode"
    This reverts commit f21276789090aed0ad4c58c312733fb1f0cd13e4.

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -11,9 +11,9 @@ return {
     },
     bigfile = { enabled = true },
     picker = {
-      -- layout = {
-      --   fullscreen = false,
-      -- },
+      layout = {
+        fullscreen = true,
+      },
     },
   },
 }
