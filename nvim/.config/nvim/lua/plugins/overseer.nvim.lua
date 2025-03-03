fdc1e4d333680ef74884e8a03f35043c54ca10dc
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-03-03 08:33:49 +0500

    overseer: open in float term instead of tab

--- nvim/.config/nvim/lua/plugins/overseer.nvim.lua
+++ nvim/.config/nvim/lua/plugins/overseer.nvim.lua
@@ -3,8 +3,8 @@ return {
   keys = {
     {
       "<leader>ol",
-      "<cmd>OverseerQuickAction open tab<cr>",
-      desc = "Open last run task in new tab",
+      "<cmd>OverseerQuickAction open float<cr>",
+      desc = "Open last run task in float",
     },
   },
 }
