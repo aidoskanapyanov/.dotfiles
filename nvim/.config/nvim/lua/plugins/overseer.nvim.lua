16e14df66b4a9645e0dff20985618e699969a630
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2025-03-03 07:57:40 +0500

    overseer: add open tab quick action keymap

---
+++ nvim/.config/nvim/lua/plugins/overseer.nvim.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/overseer.nvim",
+  keys = {
+    {
+      "<leader>ol",
+      "<cmd>OverseerQuickAction open tab<cr>",
+      desc = "Open last run task in new tab",
+    },
+  },
+}
