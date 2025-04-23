f14cf7241fb4605564895d977b1110a0abd05abd
Aidos Kanapyanov <aidos.kanapyanov@chetwood.co>
2025-04-23 15:46:48 +0500

    lspconfig: add cloudformation custom tags to yamlls

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -11,6 +11,33 @@ return {
           client.server_capabilities.documentSymbolProvider = false
         end,
       },
+      yamlls = {
+        settings = {
+          yaml = {
+            customTags = {
+              -- These are all related to AWS CloudFormation.
+              "!And sequence",
+              "!Base64",
+              "!Cidr sequence",
+              "!Equals sequence",
+              "!FindInMap sequence",
+              "!GetAZs",
+              "!GetAtt",
+              "!If sequence",
+              "!ImportValue",
+              "!Join sequence",
+              "!Length sequence",
+              "!Not sequence",
+              "!Or sequence",
+              "!Ref",
+              "!Select sequence",
+              "!Split sequence",
+              "!Sub",
+              "!Transform scalar",
+            },
+          },
+        },
+      },
     },
   },
 }
