a7aba32a5fe7c594fce78bb559830add162c2dc1
Aidos Kanapyanov <aidos.kanapyanov@chetwood.co>
2025-04-24 01:54:11 +0000

    lsp_lines: plugin to show diagnostics better (leader +vl)

---
+++ nvim/.config/nvim/lua/plugins/lsp_lines.lua
@@ -0,0 +1,17 @@
+return {
+  "Maan2003/lsp_lines.nvim",
+  config = function()
+    require("lsp_lines").setup()
+
+    vim.diagnostic.config({
+      virtual_text = true,
+      virtual_lines = false,
+    })
+
+    -- toggle for virtual lines
+    vim.keymap.set("n", "<leader>vl", function()
+      local virtual_lines_enabled = not vim.diagnostic.config().virtual_lines
+      vim.diagnostic.config({ virtual_lines = virtual_lines_enabled, virtual_text = not virtual_lines_enabled })
+    end)
+  end,
+}
