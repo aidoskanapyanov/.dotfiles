7a4ec0abc443d6e5d05fc18aeb579bd4aaa8be83
Aidos Kanapyanov <aidos.kanapyanov@chetwood.co>
2025-06-02 19:58:57 +0500

    nvim-dap-python: add chetwood-testing config

--- nvim/.config/nvim/lua/plugins/python-dap.lua
+++ nvim/.config/nvim/lua/plugins/python-dap.lua
@@ -16,5 +16,24 @@ return {
       program = vim.fn.getcwd() .. "/manage.py",
       args = { "runserver" },
     })
+
+    -- chetwwood-testing
+    table.insert(dap.configurations.python, {
+      type = "python",
+      request = "launch",
+      name = "Debug Savings_QA2 Tests",
+      program = "/home/aidos/dev/work/chetwood-testing/.venv/bin/execute-pytest",
+      env = { PYTHONPATH = vim.fn.getcwd() },
+
+      -- poetry run execute-pytest --config API_Security_Headers_X --target-env Savings_qa2 -r -c
+      args = {
+        "--config",
+        "API_Security_Headers_X",
+        "--target-env",
+        "Savings_qa2",
+        "-r",
+        "-c",
+      },
+    })
   end,
 }
