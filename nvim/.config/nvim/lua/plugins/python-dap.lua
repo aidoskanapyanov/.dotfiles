b02590a165de93e24c4868dab9174cc4e66539e8
Aidos Kanapyanov <aidos.kanapyanov@chetwood.co>
2025-09-26 03:30:57 +0500

    python-dap: remove bad chetwood-testing debugger configuration

--- nvim/.config/nvim/lua/plugins/python-dap.lua
+++ nvim/.config/nvim/lua/plugins/python-dap.lua
@@ -17,25 +17,6 @@ return {
       args = { "runserver" },
     })
 
-    -- chetwwood-testing
-    table.insert(dap.configurations.python, {
-      type = "python",
-      request = "launch",
-      name = "Debug Savings_QA2 Tests",
-      program = "/home/aidos/dev/work/chetwood-testing/.venv/bin/execute-pytest",
-      env = { PYTHONPATH = vim.fn.getcwd() },
-
-      -- poetry run execute-pytest --config API_Security_Headers_X --target-env Savings_qa2 -r -c
-      args = {
-        "--config",
-        "API_Security_Headers_X",
-        "--target-env",
-        "Savings_qa2",
-        "-r",
-        "-c",
-      },
-    })
-
     -- sls offline debugger
     table.insert(dap.configurations.python, {
       type = "python",
